eff98a03d1bd1a5a69198db13abc92ffa9252d2d
spiderVS <fuzzy003@gmail.com>
2021-04-11 23:54:40 +0300

    fix: add sort-by-height task solution

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,30 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+
+function sortByHeight(arr) {
+  const resArr = [];
+  const transArr = [];
+
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      transArr.push(arr[i]);
+    } else resArr[i] = arr[i];
+  }
+
+  transArr.sort(compareNumbers);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      resArr[i] = transArr.shift();
+    }
+  }
+
+  return resArr;
 }
 
 module.exports = sortByHeight;
