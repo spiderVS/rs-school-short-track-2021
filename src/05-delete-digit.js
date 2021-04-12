a2c5865c36a38083c5b8b4a5370bdf7fad94f761
spiderVS <fuzzy003@gmail.com>
2021-04-12 11:57:25 +0300

    fix: add delete-digit task solution

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,25 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  function compareNumbers(a, b) {
+    return b - a;
+  }
+
+  const nStr = `${n}`;
+
+  const nArr = nStr.split('');
+  let xArr = nStr.split('');
+  const kArr = [];
+
+  for (let i = 0; i < nArr.length; i++) {
+    xArr[i] = '';
+    kArr[i] = +xArr.join('');
+    xArr = nStr.split('');
+  }
+
+  kArr.sort(compareNumbers);
+  return kArr[0];
 }
 
 module.exports = deleteDigit;
